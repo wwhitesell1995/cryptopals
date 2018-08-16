8af2a47708423386750bcc40dd1087042847bead
wwhitesell1995 <whitesell1995@gmail.com>
2018-08-16 10:29:59 -0400

    Changed set_1_c2 to work with hex decoder.

--- Rust/set_1_c2/src/crypto_functions.rs
+++ Rust/set_1_c2/src/crypto_functions.rs
@@ -1,22 +1,36 @@
+//Decodes a hexidecimal string into a set of 3 bytes
+pub fn decode_hex(hex_input: &str)->Vec<u32>
+{
+    let mut bytes3: Vec<u32> = Vec::with_capacity(hex_input.len() / 6);
+    for i in 0..(hex_input.len()/6)
+    {
+        let decimal=u32::from_str_radix(&hex_input[6*i .. 6*i+6], 16).unwrap();
+        bytes3.push(decimal);
+    }
+    
+    bytes3
+}
+
+
 //Gets the xor of two hexidecimal values
-pub fn get_xor_hex(hex_input: &str, xor_string: &str) -> String
+pub fn get_xor_hex(hex_input: &str, xor_input: &str) -> String
 {
     let mut xor_output="".to_owned();
     
-    if hex_input.len()==xor_string.len()
+    if hex_input.len()==xor_input.len()
     {
-        for i in 0..hex_input.len()
+        let decimal_hex_input=decode_hex(hex_input);
+        let decimal_xor_input=decode_hex(xor_input);
+        for i in 0..decimal_hex_input.len()
         {
-            let decimal_hex_input=u64::from_str_radix(&hex_input[i..i+1], 16).unwrap();
-            let decimal_xor_string=u64::from_str_radix(&xor_string[i..i+1], 16).unwrap();
-            let xor_val=decimal_hex_input^decimal_xor_string;
+            let xor_val=decimal_hex_input[i]^decimal_xor_input[i];
             let hex_xor_val=format!("{:x}", xor_val);
             xor_output.push_str(&hex_xor_val);
         }
     }
     else
     {
-        panic!("Error! Hexidecimal strings must match!");
+        panic!("Error! Hexidecimal strings must match in length!");
     }
 
     xor_output
--- Rust/set_1_c2/target/debug/set_1_c2.d
+++ Rust/set_1_c2/target/debug/set_1_c2.d
@@ -1,1 +1,1 @@
-/home/tyler/Documents/cryptopals/Rust/set_1_c2/target/debug/set_1_c2: /home/tyler/Documents/cryptopals/Rust/set_1_c2/src/crypto_functions.rs /home/tyler/Documents/cryptopals/Rust/set_1_c2/src/main.rs
+/home/tyler/Documents/cryptopals/Rust/set_1_c2/target/debug/set_1_c2: /home/tyler/Documents/cryptopals/Rust/set_1_c2/src/main.rs /home/tyler/Documents/cryptopals/Rust/set_1_c2/src/crypto_functions.rs

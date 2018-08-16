73e690db1b521ca16971ea61fb35b564e1b4ccdf
wwhitesell1995 <whitesell1995@gmail.com>
2018-08-16 15:53:48 -0400

    Made some formatting changes for set 1 challenge 3.

--- Rust/set_1_c3/src/crypto_functions.rs
+++ Rust/set_1_c3/src/crypto_functions.rs
@@ -17,47 +17,47 @@ pub fn get_xor_hex_bytes(hex_input: &str, charcode: u8) -> Vec<u8> {
     for byte in &bytes {
         let xor_val = byte ^ charcode;
         xor_output.push(xor_val);
-   }
+    }
     xor_output
 }
 
 //Gets the number of occurences a set of chracter codes have in a set of bytes.
-fn get_score (xor_bytes: Vec<u8>) -> u32
-{
-    let mut score=0;
-    let most_used_letters="ETAOIN SHRDLU";
-    for letter in most_used_letters.chars()
-    {
-       let uppercase_charcode=letter.to_ascii_uppercase() as u8;
-       let lowercase_charcode=letter.to_ascii_lowercase() as u8;
-       
-       score += xor_bytes.iter().filter(|&n| *n == uppercase_charcode).count() as u32;
-       
-       if uppercase_charcode!=lowercase_charcode
-       {
-           score += xor_bytes.iter().filter(|&n| *n == lowercase_charcode).count() as u32;
-       }
+fn get_score(xor_bytes: Vec<u8>) -> u32 {
+    let mut score = 0;
+    let most_used_letters = "ETAOIN SHRDLU";
+    for letter in most_used_letters.chars() {
+        let uppercase_charcode = letter.to_ascii_uppercase() as u8;
+        let lowercase_charcode = letter.to_ascii_lowercase() as u8;
+
+        score += xor_bytes
+            .iter()
+            .filter(|&n| *n == uppercase_charcode)
+            .count() as u32;
+
+        if uppercase_charcode != lowercase_charcode {
+            score += xor_bytes
+                .iter()
+                .filter(|&n| *n == lowercase_charcode)
+                .count() as u32;
+        }
     }
-    
+
     score
 }
 
 //Gets the character codes for the set of bytes with the largest score.
-pub fn get_max_score_bytes(hex_input: &str) -> Vec<u8> 
-{
-    let mut max_score_charcode=0;
-    let mut max_score=0;
+pub fn get_max_score_bytes(hex_input: &str) -> Vec<u8> {
+    let mut max_score_charcode = 0;
+    let mut max_score = 0;
 
-    for i in 0..128
-    {
-        let xor_bytes=get_xor_hex_bytes(hex_input, i);
-        let score=get_score(xor_bytes);
-        if score>max_score 
-        {
-            max_score=score;
-            max_score_charcode=i;
+    for i in 0..128 {
+        let xor_bytes = get_xor_hex_bytes(hex_input, i);
+        let score = get_score(xor_bytes);
+        if score > max_score {
+            max_score = score;
+            max_score_charcode = i;
         }
     }
-    
+
     get_xor_hex_bytes(hex_input, max_score_charcode)
 }
